00da2847981c26474a03568a94cb90fd5b09f871
cuongdk253 <cuongdk253@gmail.com>
2020-12-22 14:54:17 +0700

    Initial Commit

---
+++ app2/app2.h
@@ -0,0 +1,18 @@
+//
+//  app2.h
+//  app2
+//
+//  Created by Cuong DK on 22/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for app2.
+FOUNDATION_EXPORT double app2VersionNumber;
+
+//! Project version string for app2.
+FOUNDATION_EXPORT const unsigned char app2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <app2/PublicHeader.h>
+
+
