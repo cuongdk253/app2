888f4834d73b5024682154dd4ab2491899e082f4
cuongdk253 <cuongdk253@gmail.com>
2020-12-22 15:26:19 +0700

    pull

---
+++ app2/app2.swift
@@ -0,0 +1,23 @@
+//
+//  app2.swift
+//  app2
+//
+//  Created by Cuong DK on 22/12/2020.
+//
+
+import Foundation
+
+
+public final class App2 {
+
+    let name = "App2"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
